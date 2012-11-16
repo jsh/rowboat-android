6be897b8606baf127806011df0a2b047b1f7b034
Jeffrey Haemer <jhaemer@jhaemer-lucid-a.(none)>
2012-11-16 09:35:58 -0700

    Beaglebone build

--- test-make-functions.mk
+++
@@ -1,9 +0,0 @@
-BUILD_SYSTEM=/home/jhaemer/android/aosp-2.3.x/build/core
-
-default:
-	echo $(call all-subdir-makefiles,.)
-
-include $(BUILD_SYSTEM)/combo/HOST_linux-x86.mk
-include $(BUILD_SYSTEM)/definitions.mk
-
-.PHONY: default
